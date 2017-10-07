d33d1714513d32a9b8e5240134385f33a3b9c2ce
Christian Mansch <epost@cm-informatics.de>
2017-10-07 18:19:12 +0200

    Initial Commit

---
+++ BLE/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  BLE-Test
+//
+//  Created by Christian Mansch on 07.10.17.
+//  Copyright © 2017 Christian.Mansch. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var servicesTextView: UITextView!
+    @IBOutlet weak var characteristicsTextView: UITextView!
+    
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral?
+    var headphoneTag: CBPeripheral?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        switch central.state {
+        case .poweredOn :
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print("The state is powerdOn")
+        case .poweredOff :
+            print("The state is powerdOff")
+        case .unauthorized :
+            print("The state is unauthorized")
+        case .unknown :
+            print("The state is unknown")
+        case .unsupported :
+            print("The state is unsupported")
+        case .resetting :
+            print("The state is resetting")
+        }
+        print("Scanning: \(central.isScanning)")
+        statusLabel.text = "Scanning: \(central.isScanning ? "started" : "stopped")"
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        if peripheral.name == "Aventho"
+        {
+            print("The Name is: \(peripheral.name ?? "nil")")
+            label1.text = "Connected to: \(peripheral.name!)"
+            central.isScanning ? central.stopScan() : print("scanning already stopped")
+            headphoneTag = peripheral
+            headphoneTag?.delegate = self
+            
+            if let aventho = headphoneTag {
+                central.connect(aventho, options: nil)
+            }
+        }
+        print("Scanning: \(central.isScanning)")
+        statusLabel.text? = "Scanning: \(central.isScanning ? "started" : "stopped")"
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("An Error has occoured: \(String(describing: error?.localizedDescription))")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                servicesTextView.text.append("\n\(service.uuid.uuidString)")
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if error != nil {
+            print("Error discovering Characteristics: \(String(describing: error?.localizedDescription))")
+        }
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                characteristicsTextView.text.append("\n\(characteristic.uuid)")
+                print(characteristic.uuid)
+                peripheral.readValue(for: characteristic)
+            }
+        }
+        
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let value = characteristic.value {
+            
+            let data = NSString(data: value, encoding: 8)!
+            print(data)
+            characteristicsTextView.text.append("\n\(data)")
+            
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        label1.text = "Disconnected"
+    }
+    
+ @IBAction func connect(_ sender: UIButton) {
+     centralManager.cancelPeripheralConnection(headphoneTag!)
+     }
+}
